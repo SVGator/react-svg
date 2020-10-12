5559b8ffa2e35df9016f37be099fdb9a8716091b
petri8 <petri8@svgator.net>
2020-10-12 15:49:00 +0300

    animated svg inside button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Animated from "./Animated.svg";
+import "./App.css";
 
 function App() {
+  let [color, setColor] = useState(false);
+
+  function handleClick() {
+    let svg = window.document.getElementById("mysvg");
+    let svgDocument = svg.contentDocument;
+    let svgTag = svgDocument.getElementById("efxc4pz558wj1"); // the id from the svg file
+    svgTag.dispatchEvent(new Event("click"));
+
+    console.log("It's working");
+    setColor(!color);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <button className={color ? "null" : "button"} onClick={handleClick}>
+      <object id="mysvg" type="image/svg+xml" data={Animated}>
+        svg-animation
+      </object>
+    </button>
   );
 }
-
 export default App;
